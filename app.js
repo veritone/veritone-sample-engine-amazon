da78bced06dcc26353f517726971120d0df058a7
JerYang <jyang@veritone.com>
2018-01-17 14:53:55 -0800

    App clean up

--- app.js
+++ app.js
@@ -35,14 +35,13 @@ if (payload_file != null) {
     try {
         payload = JSON.parse(params);
     } catch (_error) {
-        e = _error;
         try {
             parsed = querystring.parse(params);
             if (!(Object.keys(parsed).length === 1 && parsed[Object.keys(parsed)[0]] === '')) {
                 payload = parsed;
             }
         } catch (_error) {
-            e = _error;
+            throw new Error('Unable to parse payload file!');
         }
     }
 }
@@ -52,15 +51,12 @@ if (config_file != null) {
     try {
         config = JSON.parse(config);
     } catch (_error) {
-        e = _error;
+        throw new Error('Unable to parse config file!');
     }
 }
 
 if (typeof config !== 'object') {
     throw new Error('Missing config!');
-}
-if (typeof config['veritone-api'] !== 'object') {
-    throw new Error('Missing veritone-api config!');
 }
 if (typeof payload !== 'object') {
     throw new Error('Missing payload!');
@@ -74,10 +70,20 @@ if (typeof payload.jobId !== 'string') {
 if (typeof payload.taskId !== 'string') {
     throw new Error('Missing from payload taskId!');
 }
-
 if (typeof payload.recordingId !== 'string') {
     throw new Error('Missing from payload recordingId!');
 }
+if (typeof payload.token !== 'string') {
+    throw new Error('Missing from payload token!');
+}
+if (typeof payload.veritoneApiBaseUrl !== 'string') {
+    throw new Error('Missing from payload veritoneApiBaseUrl!');
+}
+
+//replace with
+config["veritone-api"]["token"] = payload.token;
+config["veritone-api"]["baseUri"] = payload.veritoneApiBaseUrl;
+config["graphql"]["baseUri"] = payload.veritoneApiBaseUrl+"/v3/graphql";
 
 var awsConfig = config['aws'];
 if (typeof awsConfig !== 'object') {
@@ -87,17 +93,11 @@ if (typeof awsConfig !== 'object') {
 if (!isNaN(payload.threshold)) {
     awsConfig.threshold = payload.threshold;
 }
-if (!isNaN(payload.maxNumResults)) {
-    awsConfig.maxNumResults = payload.maxNumResults;
-}
-if (!isNaN(payload.clumpThreshold)) {
-    config.clumpThreshold = payload.clumpThreshold;
-}
 if (!isNaN(payload.framesPerSecond)) {
     config.framesPerSecond = payload.framesPerSecond;
 }
 if (!isNaN(payload.minConfidence)) {
- config.minConfidence = payload.minConfidence;
+    config.minConfidence = payload.minConfidence;
 }
 if (!isNaN(payload.maxParallelImages)) {
     config.maxParallelImages = payload.maxParallelImages;
@@ -106,12 +106,7 @@ if (!isNaN(payload.maxParallelImages)) {
 if (!config.minConfidence) config.minConfidence = 98;
 
 // used only to tune engine. does not affect results.
-// limits concurrent async processing of images as a sanity check so
-// that we don't attempt to load too much into memory at once.
 if (!config.maxParallelImages) config.maxParallelImages = 50;
-
-config["veritone-api"]["token"] = payload.token;
-var inDevelopment = process.env.NODE_ENV != "production";
 
 const assetApi = require('./asset.js')(config);
 
@@ -122,11 +117,9 @@ function startTask(callback) {
     const task = { taskStatus: 'running' };
     veritoneClient.updateTask(payload.jobId, payload.taskId, task, function startTaskCompleteCallback(err) {
         if (err) {
-            if (!inDevelopment) {
-                console.log('jobId=' + payload.jobId + ', taskId=' + payload.taskId);
-                console.log('update task failed: ' + util.inspect(err));
-                return failTask('updateTaskToRunning.error ' + util.inspect(err));
-            }
+            console.log('jobId=' + payload.jobId + ', taskId=' + payload.taskId);
+            console.log('update task failed: ' + util.inspect(err));
+            return failTask('updateTaskToRunning.error ' + util.inspect(err));
         }
 
         console.log('updateTaskToRunning.success');
@@ -208,7 +201,6 @@ function createAsset(transformedData, callback) {
     let assetContent = JSON.stringify(transformedData, null, 2);
     assetApi.createAsset(payload.recordingId, contentType, assetType, fileName, source, assetContent, callback);
 }
-
 
 // Main
 console.log(".. Engine starting...");
